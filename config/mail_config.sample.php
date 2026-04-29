8fd6b711c50f4d1a625b0f169b34ccae38feb0cc
F1D02310067_KEVIN MUAMMAR GATHFAN <kevin.gaffan5@gmail.com>
2026-04-29 20:34:07 +0800

    adjustment

--- config/mail_config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'from_name' => 'Web Perpus Admin',
-    'from_email' => 'kevinmuammargathfan@gmail.com',
-    'brevo_api_key' => 'ISI_API_KEY_ANDA_DI_SINI', 
-    
-    // URL dasar aplikasi (sesuaikan jika pindah domain)
-    'app_url' => 'https://kuisioner-disarpus.page.gd'
-];
