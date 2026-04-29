df09a99e4db903b8e938da3d4726be3304804b06
kevingathfan <kevinmuammargathfan@gmail.com>
2026-04-29 20:39:55 +0800

    adjustment

--- config/mail_config.php
+++ config/mail_config.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'from_name' => 'Web Perpus Admin',
-    'from_email' => 'kevinmuammargathfan@gmail.com',
-    'brevo_api_key' => 'API_KEY', // ISI MANUAL DI SERVER 
+    'from_email' => 'EMAIL_BREVO',
+    'brevo_api_key' => 'API_KEY_BREVO', // ISI MANUAL DI SERVER 
     
     // URL dasar aplikasi (sesuaikan jika pindah domain)
-    'app_url' => 'https://kuisioner-disarpus.page.gd'
+    'app_url' => 'URL_ANDA'
 ];
