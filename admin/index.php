bc4bdba65288f1f8b64d5a033ec02648fca85d64
kevingathfan <kevinmuammargathfan@gmail.com>
2026-06-16 01:10:38 +0800

    update MVC structure

--- admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Redirect direct /admin/ access to login page.
-header('Location: login.php');
-exit;
--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// admin/logout.php
-session_start();
-session_unset();
-session_destroy();
-header('Location: login.php');
-exit;
